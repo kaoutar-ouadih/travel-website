8659f8056258078d187555b7eb6647d8bff7e866
SalmaHaddoute <salmahaddoute@gmail.com>
2024-11-08 22:12:52 +0100

    last changes

--- src/pages/Details/ProductDetailsPage.tsx
+++ src/pages/Details/ProductDetailsPage.tsx
@@ -1,4 +1,4 @@
-
+import AccountNav from "../../compenants/AccountNav/AccountNav"
 import AvailableRooms from "../../compenants/AvailableRooms/AvailableRooms"
 import CovidRestrictions from "../../compenants/CovidRestrictions/CovidRestrictions"
 import ProductDetails from "../../compenants/ProductDetails/ProductDetails"
@@ -7,6 +7,7 @@ import ProductDetails from "../../compenants/ProductDetails/ProductDetails"
 const ProductDetailsPage = () => {
   return (
     <div>
+      <AccountNav/>
       <ProductDetails/>
       <AvailableRooms/>
       <CovidRestrictions/>
