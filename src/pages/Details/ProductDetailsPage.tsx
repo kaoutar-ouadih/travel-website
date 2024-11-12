29afea5b45c3135ef85b9e6ee80dd1a6e8689602
kaoutarOuadih1 <kaoutar.ouadih8@gmail.com>
2024-11-12 13:31:29 +0100

    some code enhancement

--- src/pages/Details/ProductDetailsPage.tsx
+++ src/pages/Details/ProductDetailsPage.tsx
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
 import AccountNav from "../../compenants/AccountNav/AccountNav"
->>>>>>> 8659f8056258078d187555b7eb6647d8bff7e866
 import AvailableRooms from "../../compenants/AvailableRooms/AvailableRooms"
 import CovidRestrictions from "../../compenants/CovidRestrictions/CovidRestrictions"
 import ProductDetails from "../../compenants/ProductDetails/ProductDetails"
